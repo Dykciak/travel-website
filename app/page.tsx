725db4b0917b649070a6a00afd8960f575a62a16
dykciak <dawid.dykto@gmail.com>
2025-03-30 22:06:18 +0200

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Hero from "@/components/Hero";
+import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
 
 export default function Home() {
 	return (
 		<>
-			
+			<Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
 		</>
 	);
 }
